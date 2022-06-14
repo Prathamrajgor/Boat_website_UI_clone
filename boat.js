1cc2000644344b1a3f8b2ddd3395ab5c46c3bf3c
Pratham <prathamrajgor@gmail.com>
2022-06-14 12:09:48 +0530

    1

---
+++ boat.js
@@ -0,0 +1,36 @@
+pr1.addEventListener("click",atc1=()=>{
+    let cost1=document.getElementById("pr1").innerText;
+    console.log("akdghkg");
+})
+pr2.addEventListener("click",atc1=()=>{
+    let cost1=document.getElementById("pr2").innerText;
+    console.log("akdghkg");
+})
+pr3.addEventListener("click",atc1=()=>{
+    let cost1=document.getElementById("pr3").innerText;
+    
+})
+pr4.addEventListener("click",atc1=()=>{
+    let cost1=document.getElementById("pr4").innerText;
+    console.log("akdghkg");
+})
+pr5.addEventListener("click",atc1=()=>{
+    let cost1=document.getElementById("pr5").innerText;
+    console.log("akdghkg");
+})
+pr6.addEventListener("click",atc1=()=>{
+    let cost1=document.getElementById("pr6").innerText;
+    console.log("akdghkg");
+})
+pr7.addEventListener("click",atc1=()=>{
+    let cost1=document.getElementById("pr7").innerText;
+    console.log("akdghkg");
+})
+pr8.addEventListener("click",atc1=()=>{
+    let cost1=document.getElementById("pr8").innerText;
+    console.log("akdghkg");
+})
+pr9.addEventListener("click",atc1=()=>{
+    let cost1=document.getElementById("pr9").innerText;
+    console.log("akdghkg");
+})
